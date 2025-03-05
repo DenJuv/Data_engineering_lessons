cbd517a4c474a3f48f82b4de2868c60b7dcc8e69
Quasar <juveredden@gmail.com>
2025-03-05 21:54:54 +0300

    test

--- Test.py
+++ Test.py
@@ -1,4 +1,16 @@
-q=1
-a=2
-w=q+a
-print(w)
+import requests
+from datetime import datetime
+
+api_key = '5f8926889b22493da19dd7fef13fed60'
+curr = 'RUB,EUR'
+sd = '2025-03-03'
+ed = '2025-03-05'
+URL = f'http://currencylayer.com/change?access_key={api_key}&currencies={curr}&start_date={sd}&end_date={ed}'
+r=requests.get(url=URL)
+result = r.json()
+print(result)
+#dt = datetime.fromtimestamp(result.get('timestamp'))
+#dt = dt.strftime('%Y-%m-%d')
+#print(dt)
+print(result.get('quotes').get('USDRUB'))
+print(result.get('quotes').get('USDEUR'))
