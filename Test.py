cb27eafc74f0cb5e5b7f170def5ec58c2fb776f4
Quasar <juveredden@gmail.com>
2025-03-05 21:59:54 +0300

    test

--- Test.py
+++ Test.py
@@ -3,14 +3,16 @@ from datetime import datetime
 
 api_key = '5f8926889b22493da19dd7fef13fed60'
 curr = 'RUB,EUR'
-sd = '2025-03-03'
+sd = '2024-03-03'
 ed = '2025-03-05'
-URL = f'http://currencylayer.com/change?access_key={api_key}&currencies={curr}&start_date={sd}&end_date={ed}'
-r=requests.get(url=URL)
+URL = f'http://api.currencylayer.com/change?access_key={api_key}&currencies={curr}&start_date={sd}&end_date={ed}'
+r = requests.get(url=URL)
 result = r.json()
 print(result)
 #dt = datetime.fromtimestamp(result.get('timestamp'))
 #dt = dt.strftime('%Y-%m-%d')
 #print(dt)
+print(result.get('start_date'))
+print(result.get('end_date'))
 print(result.get('quotes').get('USDRUB'))
 print(result.get('quotes').get('USDEUR'))
